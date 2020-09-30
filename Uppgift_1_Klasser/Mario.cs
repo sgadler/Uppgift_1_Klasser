7b9eed2195c8158d11e42a39971ab7d1b31a4f07
Sebastian <gadlersebastian@gmail.com>
2020-09-30 10:09:22 +0200

    I am Trying something but im not sure where im going with it

---
+++ Uppgift_1_Klasser/Mario.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Uppgift_1_Klasser
+{
+    public class Mario
+    {
+        public int x = 0;
+        public int y = 0;
+
+        public int lifePoints = 2;
+
+        public bool isDead = false; 
+    }
+}
---
+++ Uppgift_1_Klasser/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Uppgift_1_Klasser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                
+        }
+    }
+}
